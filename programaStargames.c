595f90ebef8186d54473158bd7c4e9b12fdc9a54
alejandrogomez27 <80398541+alejandrogomez27@users.noreply.github.com>
2021-05-21 11:21:51 +0200

    Update programaStargames.c

--- programaStargames.c
+++ programaStargames.c
@@ -1,4 +1,8 @@
+
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main () {
 	
@@ -7,8 +11,8 @@ int main () {
 
 	do{
 	printf("BIENVENIDO AL MENU DE STARGAMES \n");
-	printf("J (Seleccionar juego), R (Reglas del juego), 4 (Salir del programa) \n");
 	printf("Introduzca numero del juego que desea jugar:\n");
+	
 	scanf("%c",&opcion);
 	
 	switch (opcion){
@@ -16,8 +20,8 @@ int main () {
 			printf("J1 introduzca su nombre de usuario:\n");
 			fflush(stdin);
 			scanf("%c",&jugador1);
-			printf("Es un juego basado en aumentar el tamaño de la serpiente , solo puede jugar un jugador.\n");
-		        printf("El usuario tendra tres niveles de dificultad:facil,medio,dificil.\n");
+			printf("Es un juego basado en aumentar la longitud de la serpiente , solo puede jugar un jugador.\n");
+		     printf("El usuario tendra tres niveles de dificultad:facil,medio,dificil.\n");
 			printf("Si el usuario completa el juego le saldra un mensaje de reiniciaro o volver al menu del programa.\n");
 			printf("Si el usuario choca con alguna de las paredes le aparecera un mensaje de GAME OVER y la opcion de volver a empezar.\n");
 			break;
@@ -28,31 +32,24 @@ int main () {
 			fflush(stdin);
 			scanf("%c",&jugador1);
 		    printf("Es un juego que trata de adivinar ciertas palabras con un numero de oportunidades.\n");
-			printf("El usuario tendra que decir una letra de abecedario en cad turno para intentar deducir la palabra,");
-			printf("pero no solo tiene 6 oportunidades,sino fallara el juego\n");
-			printf("Cada vez que una letra dicha no aparezca en la palabra,se le restara una oportunidad y aparecera una notificacion con las restantes.\n");
-			printf("Si tambien falla al intentar adivinar la palabra, contara tambien como oportunidad perdida.\n");
-			printf("Los temas de este juego son:\n");
-			printf(" -Famosos.\n");
-			printf(" -Ciudades.\n");
-			printf(" -Paises.\n");
+			printf("El usuario tendra que decir una letra del abecedario en cada turno para intentar deducir la palabra,");
+			printf("pero solo tiene 5 oportunidades,sino fallara el juego\n");
+			printf("Cada vez que una letra dicha no aparezca en la palabra,se le restara una oportunidad .\n");
 			printf("Cuando el jugador acierta la palabra el juego mostrara un mensaje de VICTORIA!!! ,\n");
 			printf(" si el jugador no acierta la palabra y no tiene oportunidades restantes aparecera un mensaje de FIN DE JUEGO.\n");
 			printf("En ambos casos se mostrara la opcion de volver al menu o reiniciar el juego\n");
 			break;
 	
-			  
+		
 	    case'3'://Abandonar juego
 			printf("Hasta la proxima!!!!!!!!\n");
-			default:
-		printf("la opcion es incorrecta:\n");
+			break;
+		default:
+		    printf("la opcion es incorrecta:\n");
+		    break;
 			return 0;//salir del programa
 	}
 		
 }while (juego1||juego2);
 		
 	}
-
-	
-	
-
